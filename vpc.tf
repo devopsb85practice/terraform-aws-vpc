ef9be0e27f94b9a3a97c8add34eb90bb1c98fba7
lakshiprasannavelaga <prasanna.v373@gmail.com>
2025-09-01 21:20:25 -0400

    practice

---
+++ vpc.tf
@@ -0,0 +1,14 @@
+#roboshop-dev/prod
+
+resource "aws_vpc" "roboshop" {
+  cidr_block       = var.cidr_block
+  instance_tenancy = "default"
+  enable_dns_hostnames = "true"
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}"
+    }
+  )
+}

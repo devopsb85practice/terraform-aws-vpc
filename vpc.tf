ed0bfed85dccdbd4b08349936b9bd0a0c7a6dde6
lakshiprasannavelaga <prasanna.v373@gmail.com>
2025-09-02 21:34:09 -0400

    practice

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,20 @@
 #roboshop-dev/prod
 
-resource "aws_vpc" "vpc" {
+resource "aws_vpc" "main" {
   cidr_block       = var.cidr_block
   instance_tenancy = "default"
   enable_dns_hostnames = "true"
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}"
+    }
+  )
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id  #attatch the VPC
 
   tags = merge(
     local.common_tags,
